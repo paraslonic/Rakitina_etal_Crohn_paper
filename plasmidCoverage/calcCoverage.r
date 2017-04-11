93426e185997c97a2ccbea9464eda7e017322a3c
paraslonic <paraslonic@gmail.com>
2017-04-11 16:16:39 +0300

    rename

--- plasmidCoverage/calcCoverage.r
+++ plasmidCoverage/calcCoverage.r
@@ -14,8 +14,10 @@ for (f in files){
 }
 
 cov = unlist(cov.list)
+pdf("plf82_coverage.pdf")
 par(mar=c(5.1,12,4.1,2.1))
 barplot(cov, las=2, col = "dodgerblue3", cex.names=  0.6, hor=TRUE, xlab="pLF82 percent coverage")
+dev.off()
 cov = data.frame(names(cov),cov)
 colnames(cov) = c("strain","plasmid coverage")
 write.csv(cov,"coverage.csv", row.names=FALSE)
