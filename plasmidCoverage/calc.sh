f77730e12e05ef39d2e7a8db934d00d9b44c4171
paraslonic <paraslonic@gmail.com>
2017-04-11 15:29:17 +0300

    add plasmid coverage

---
+++ plasmidCoverage/calc.sh
@@ -0,0 +1,6 @@
+mkdir blastout
+for f in ref/*.fasta
+do 
+	name=$(basename $f .fasta)
+	blastn -query plf82.fasta -subject $f -outfmt '6 std qlen slen qcovs' -evalue 1e-5 > blastout/$name
+done
---
+++ plasmidCoverage/calcCoverage.r
@@ -0,0 +1,21 @@
+library("IRanges")
+
+path = "blastout/"
+ref.length = 108379
+files = dir(path)
+
+cov.list =  list()
+for (f in files){
+  t = read.delim(paste0(path,f), head = FALSE)
+  ranges = reduce(IRanges(start = t$V7, end = t$V8))
+  sum.length = sum(width(ranges))
+  coverage = sum.length/ref.length
+  cov.list[[f]]=coverage
+}
+
+cov = unlist(cov.list)
+par(mar=c(5.1,12,4.1,2.1))
+barplot(cov, las=2, col = "dodgerblue3", cex.names=  0.6, hor=TRUE, xlab="pLF82 percent coverage")
+cov = data.frame(names(cov),cov)
+colnames(cov) = c("strain","plasmid coverage")
+write.csv(cov,"coverage.csv", row.names=FALSE)
